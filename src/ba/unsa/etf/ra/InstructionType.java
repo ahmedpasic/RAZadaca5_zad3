f471da838f7dc23dc37d3fdb5829a9187d4c28bc
Belmin Selimovic <bselimovic1@etf.unsa.ba>
2020-04-26 20:49:29 +0200

    Dodao slucaj C trazenja zadrske, dodao ukoliko nema grananja da ispise da ne treba funkcija zadrske

--- src/ba/unsa/etf/ra/InstructionType.java
+++
@@ -1,7 +0,0 @@
-package ba.unsa.etf.ra;
-
-public enum InstructionType {
-
-    R_TIP, I_TIP, J_TIP
-
-}
--- src/ba/unsa/etf/ra/Main.java
+++ src/ba/unsa/etf/ra/Main.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Scanner ulaz = new Scanner(System.in);
 
@@ -16,6 +16,17 @@ public class Main {
         String put = ulaz.nextLine();
 
         ArrayList<Instruction> sekvenca = dajSekvencuIzDatoteke(put);
+
+        if(provjeriPotrebuInstrZadrske(sekvenca)==false){
+            try {
+                BufferedWriter bw = new BufferedWriter(new FileWriter("instrukcijeZadrske.txt"));
+                bw.write("S obzirom da nema niti jedne instrukcije grananja, instrukcije zadrške nisu potrebne.");
+                bw.close();
+                return;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         Map<Instruction, Instruction> parovi = dajInstrukcijeZadrske(sekvenca);
 
@@ -25,39 +36,84 @@ public class Main {
     }
 
     private static void zapisiInstrukcijeZadrske(Map<Instruction,Instruction> parovi) {
+        if (parovi != null) {
+            try {
+                BufferedWriter bw = new BufferedWriter(new FileWriter("instrukcijeZadrske.txt"));
+                for (Map.Entry<Instruction, Instruction> par : parovi.entrySet()) {
+                    Instruction inst = par.getKey();
+                    bw.write(inst.getCijelaInstrukcija());
 
-        try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter("instrukcijeZadrske.txt"));
-            for(Map.Entry<Instruction, Instruction> par : parovi.entrySet()) {
-                Instruction inst = par.getKey();
-                bw.write(inst.getCijelaInstrukcija());
+                    if (par.getValue() == null) {
+                        bw.write("-----> nema instrukciju zadrske");
+                    } else
+                        bw.write("----->" + par.getValue().getCijelaInstrukcija());
 
-                if(par.getValue() == null) {
-                    bw.write("-----> nema instrukciju zadrske");
+                    bw.newLine();
                 }
-                else
-                    bw.write("----->" + par.getValue().getCijelaInstrukcija());
-
-                bw.newLine();
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            bw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-
     private static Map<Instruction, Instruction> dajInstrukcijeZadrske(ArrayList<Instruction> sekvenca) {
-        //ovo treba implementirati
-        return null;
+        Map<Instruction, Instruction> parovi = new HashMap<Instruction, Instruction>();
+        Instruction zadrska=null;
+        for(Instruction in : sekvenca){
+            if(in.getNaziv().equals("BEQ") || in.getNaziv().equals("BNE")){
+                zadrska = dajInstrNakonInstrGrananjaZadrska(in, sekvenca);
+                if (zadrska!=null) parovi.put(in,zadrska);
+                if(zadrska==null){ //slucaj B
+                    //zadrska = funkcijaKojaDajeInstrZaSlucajB
+                    if(zadrska!=null) parovi.put(in,zadrska);
+                }
+                if(zadrska==null){
+                    //zadrska= funkcijaKojaDajeInstrZaSlucajA
+                    if(zadrska!=null) parovi.put(in,zadrska);
+                }
+                if(zadrska==null){
+                    //Ako je nakon 3 ispitana slucaja nije moguce naci zadrsku onda napisati u datoteku da za tu instrukciju
+                    //nije moguce naci zadrsku, trebalo bi znaci par instrucija, i instrukcija zadrske u kojoj pise da nije moguce naci zadrsku
+                }
+            }
+        }
+        return parovi;
+    }
+    private static Instruction dajInstrNakonInstrGrananjaZadrska(Instruction instrukcija, ArrayList<Instruction> sekvenca){
+        Instruction instr = null;
+        for(int i=0;i<sekvenca.size();i++){
+            if(sekvenca.get(i).getCijelaInstrukcija().equals(instrukcija.getCijelaInstrukcija())){
+                if(i+1==sekvenca.size()) return null; //ako je zadnja instrukcija, onda nema instrukcije iza nje
+                //ako je iza beq ili bne onda one ne bismo trebali koristiti kao instrukcije zadrske
+                if(sekvenca.get(i+1).getNaziv().equals("BEQ") || sekvenca.get(i+1).getNaziv().equals("BNE")) return null;
+                instr = sekvenca.get(i+1);
+                int j=0;
+                while(j<sekvenca.size()){
+                    if(sekvenca.get(j).getLabel()==null){
+                        j++;// && sekvenca.get(j).getLabel()!=null || !sekvenca.get(j).getLabel().equals(instrukcija.getIzvorni2()) )) j++;
+                    }
+                    else if(sekvenca.get(j).getLabel().equals(instrukcija.getIzvorni2())) break;
+                    else j++;
+                }
+                if(j==sekvenca.size()) return null; //dosli smo do kraja, a nema labele
+                while(j<sekvenca.size() && sekvenca.get(j).getCijelaInstrukcija()!=instrukcija.getCijelaInstrukcija()){
+                    if( (sekvenca.get(j).getIzvorni1()!=null && sekvenca.get(j).getIzvorni1().equals(instr.getOdredisni())) || (
+                            sekvenca.get(j).getIzvorni2()!=null && sekvenca.get(j).getIzvorni2().equals(instr.getOdredisni()) )) return null;
+                    j++;
+                }
+                if(j==sekvenca.size()) i=sekvenca.size();
+            }
+        }
+        return instr;
     }
 
-    private static ArrayList<Instruction> dajSekvencuIzDatoteke(String put) {
+    private static ArrayList<Instruction> dajSekvencuIzDatoteke(String put) throws IOException {
 
         ArrayList<Instruction> sekvenca = new ArrayList<>();
-
+        BufferedReader br = null;
         try {
 
-            BufferedReader br = new BufferedReader(new FileReader(put));
+            br = new BufferedReader(new FileReader(put));
             String red = br.readLine();
             while(red!=null) {
                 sekvenca.add(dajInstrukcijuIzStringa(red));
@@ -66,18 +122,20 @@ public class Main {
 
         } catch (Exception e) {
             e.printStackTrace();
+            br.close();
         }
+        br.close();
         return sekvenca;
+
     }
 
     private static Instruction dajInstrukcijuIzStringa(String red) {
         String label=null, naziv=null, odredisni=null, izvorni1=null, izvorni2=null, imm=null, tip=null;
-        InstructionType instructionType;
 
         List<String> dijelovi = Arrays.asList(red.split(" "));
 
         if(dijelovi.get(0).contains(":")){
-            label = dijelovi.get(0).substring(0,dijelovi.get(0).length()-1);
+            label = dijelovi.get(0).toUpperCase().substring(0,dijelovi.get(0).length()-1);
             naziv = dijelovi.get(1).trim().toUpperCase();//sve u velika tako da je podrzano citanje instrukcija i sa malim i sa velikim slovima
             if(rTip.contains(naziv)){  //ako je R tip u pitanju
                 odredisni = dijelovi.get(2).trim().toUpperCase().substring(0,dijelovi.get(2).length()-1);
@@ -97,6 +155,10 @@ public class Main {
                     izvorni1 = dijelovi.get(3).trim().toUpperCase().substring(0,dijelovi.get(3).length()-1);
                     izvorni2=null;
                     imm = dijelovi.get(4).trim().toUpperCase();
+                    if(naziv.equals("BEQ") || naziv.equals("BNE")){
+                        izvorni2=dijelovi.get(4).trim().toUpperCase();
+                        imm=null;
+                    }
                 }
                 tip="I";
             }
@@ -108,7 +170,14 @@ public class Main {
                 tip = "J";
             }
             else{ // AKO NEMA INSTRUKCIJE NAPISATI U DATOTEKU DA NE VALJA SEKVENCA I PREKINUTI SA RADOM
-
+                try {
+                    BufferedWriter bw = new BufferedWriter(new FileWriter("instrukcijeZadrske.txt"));
+                    bw.write("S obzirom da nema niti jedne instrukcije grananja, instrukcije zadrške nisu potrebne.");
+                    bw.close();
+                    System.exit(0);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
         else {//ako prvi dio nema dvotacku onda nemamo labelu i prvi dio je onda ustvari naziv instrukcije
@@ -132,6 +201,10 @@ public class Main {
                     izvorni1 = dijelovi.get(2).trim().toUpperCase().substring(0,dijelovi.get(2).length()-1);
                     izvorni2=null;
                     imm = dijelovi.get(3).trim().toUpperCase();
+                    if(naziv.equals("BEQ") || naziv.equals("BNE")){
+                        izvorni2=dijelovi.get(3).trim().toUpperCase();
+                        imm=null;
+                    }
                 }
                 tip="I";
             }
@@ -143,17 +216,18 @@ public class Main {
                 tip = "J";
             }
             else{ // AKO NEMA INSTRUKCIJE NAPISATI U DATOTEKU DA NE VALJA SEKVENCA I PREKINUTI SA RADOM
-
+                try {
+                    BufferedWriter bw = new BufferedWriter(new FileWriter("instrukcijeZadrske.txt"));
+                    bw.write("S obzirom da nema niti jedne instrukcije grananja, instrukcije zadrške nisu potrebne.");
+                    bw.close();
+                    System.exit(0);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
-
-
-
-
-
-
-        return new Instruction(label, naziv, odredisni, izvorni1, izvorni2, imm, tip, red);
+        return new Instruction(label, naziv, odredisni, izvorni1, izvorni2, imm, tip, red.toUpperCase());
     }
 
     public static String dajRegistarLwSw(String nizSlova){
@@ -168,6 +242,14 @@ public class Main {
             }
         }
         return registar.toUpperCase();
+    }
+
+    public static boolean provjeriPotrebuInstrZadrske(ArrayList<Instruction> sekvenca){
+        for(Instruction instr : sekvenca){
+            if(instr.getNaziv().equals("BEQ") || instr.getNaziv().equals("BNE"))
+                return true;
+        }
+        return false;
     }
 
     //R TIP
